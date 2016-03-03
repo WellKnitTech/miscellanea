ae03f781cf454c375edcf98c14ec10b578d75bac
Jacob <jdub01010101@gmail.com>
2016-03-03 10:36:15 -0600

    Create .wifi_login_details.sh

---
+++ .wifi_login_details.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#login details for the Uniwifi are here
+
+ssid=""
+firstname="fn"
+lastname="ln"
+email="example@example.com"
+#modify for specific environment
+post_url="http://10.100.109.160:1111/usg/process?OS=http://www.google.com"
